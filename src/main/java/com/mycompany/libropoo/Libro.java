e1752aa348133b2bd7f8ab32f42d4b4cea86079f
AngelicaG231 <maria.garzonm@konradlorenz.edu.co>
2024-04-24 23:27:02 -0500

    Actividad

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,34 @@
 package com.mycompany.libropoo;
 
 public class Libro {
-    
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int anoPublicacion;
+    private boolean leido;
+
+    public Libro(String titulo, String autor, String genero, int anoPublicacion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.anoPublicacion = anoPublicacion;
+        this.leido = false;
+    }
+ 
+    public String mostrarInformacion() {
+        return "Título: " + titulo + ", Autor: " + autor + ", Año de publicación: " + anoPublicacion + ", Género: " + genero;
+    }
+
+    public void marcarLeido() {
+        this.leido = true;
+    }
+  
+    public boolean esAntiguo() {
+        int year = java.time.Year.now().getValue();
+        return (year - anoPublicacion) > 50;
+    }
+
+    public boolean estaLeido() {
+        return leido;
+    }
 }
